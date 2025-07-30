2f34adb601ba72a315d2556ae3807b2dce5db091
João Victor Santos de Assis <joao.victor@mytech.com.br>
2025-07-30 20:22:04 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_AjYXaWa8a', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '15un2sg9m0cnlc74bu2bo15rie', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_Ae2qkZpWG', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '13v9qh3fgdkcdtfm2ruoh8isga', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        clientSecret: '1ea60bafpueoq5t2u6ipbrb4806l4q1ajdmh60k78c88pmcp87is', // e.g. abc123xyz456pqr789stuv0wxyzabcde12345fgh678ijklmnopqr
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
