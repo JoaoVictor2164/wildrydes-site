e63d4bb211c679ec4171847c4ff56d77de8768f0
João Victor Santos de Assis <joao.victor@mytech.com.br>
2025-07-30 21:15:45 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = { 
     cognito: {
         userPoolId: 'us-east-1_Ae2qkZpWG', // ID do User Pool
-        userPoolClientId: '13v9qh3fgdkcdtfm2ruoh8isga', // Client ID do User Pool
+        userPoolClientId: 'js861u44nd3vm0gs0nt9b6pdm', // Client ID do User Pool
         region: 'us-east-1' // Região do User Pool
     },
     api: {
